1ccf222cb50846d1e67197140d9ea5447fbca67f
Daniel Schulz <faulolio@gmail.com>
2022-04-26 17:49:40 +0200

    Add renderer/model stub

---
+++ src/main/java/net/slothsoft/minecraft/sloth/SlothModel.java
@@ -0,0 +1,24 @@
+package net.slothsoft.minecraft.sloth;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+
+import net.minecraft.client.model.EntityModel;
+
+public class SlothModel<T extends SlothEntity> extends EntityModel<T> {
+
+	@Override
+	public void setupAnim(T p_102618_, float p_102619_, float p_102620_, float p_102621_, float p_102622_,
+			float p_102623_) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void renderToBuffer(PoseStack p_103111_, VertexConsumer p_103112_, int p_103113_, int p_103114_,
+			float p_103115_, float p_103116_, float p_103117_, float p_103118_) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/net/slothsoft/minecraft/sloth/SlothRenderer.java
@@ -0,0 +1,21 @@
+package net.slothsoft.minecraft.sloth;
+
+import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.resources.ResourceLocation;
+
+public class SlothRenderer<T extends SlothEntity> extends MobRenderer<T, SlothModel<T>> {
+	
+	static final ResourceLocation TEXTURE = new ResourceLocation(Constants.MOD_ID, "textures/entities/sloth.png");
+
+	public SlothRenderer(Context context, SlothModel<T> model, float p_174306_) {
+		super(context, model, p_174306_);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ResourceLocation getTextureLocation(T entity) {
+		return TEXTURE;
+	}
+
+}
