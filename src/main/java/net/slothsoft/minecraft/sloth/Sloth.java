a3d474dc4c45b61f02c07bfd79afe2334e1971a3
Daniel Schulz <faulolio@gmail.com>
2022-04-26 17:41:05 +0200

    Add breeding, goals

--- src/main/java/net/slothsoft/minecraft/sloth/Sloth.java
+++ src/main/java/net/slothsoft/minecraft/sloth/Sloth.java
@@ -3,7 +3,9 @@ package net.slothsoft.minecraft.sloth;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.goal.TemptGoal;
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 
 public class Sloth extends Animal {
@@ -14,8 +16,12 @@ public class Sloth extends Animal {
 	}
 
 	@Override
-	public AgeableMob getBreedOffspring(ServerLevel level, AgeableMob mob) {
-		// TODO Auto-generated method stub
-		return null;
+	public AgeableMob getBreedOffspring(ServerLevel level, AgeableMob parent) {
+		return MobInit.SLOTH.get().create(level);
+	}
+
+	@Override
+	protected void registerGoals() {
+		goalSelector.addGoal(0, new TemptGoal(this, 100, Ingredient.of(ItemInit.EXAMPLE_BLOCK.get()), false));
 	}
 }
