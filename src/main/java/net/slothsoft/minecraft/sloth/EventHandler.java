54c5a64d5803ea677732bf56b7df2a3a92ca15b2
Nico Auerswald <auersw.nico@gmail.com>
2022-06-07 17:49:29 +0200

    added baisc animal spawnrules for Sloths

--- src/main/java/net/slothsoft/minecraft/sloth/EventHandler.java
+++ src/main/java/net/slothsoft/minecraft/sloth/EventHandler.java
@@ -2,9 +2,12 @@ package net.slothsoft.minecraft.sloth;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class EventHandler {
@@ -13,5 +16,11 @@ public class EventHandler {
 		var mc = Minecraft.getInstance();
 
 		mc.gui.getChat().addMessage(new TextComponent("Hello World!"));
+	}
+
+	@SubscribeEvent
+	public static void commonSetup(FMLCommonSetupEvent event) {
+		event.enqueueWork(()-> SpawnPlacements.register(MobInit.SLOTH.get(),
+				SpawnPlacements.Type.ON_GROUND, Heightmap.Types.WORLD_SURFACE, SlothEntity::canSpawn));
 	}
 }
--- src/main/java/net/slothsoft/minecraft/sloth/SlothEntity.java
+++ src/main/java/net/slothsoft/minecraft/sloth/SlothEntity.java
@@ -1,12 +1,19 @@
 package net.slothsoft.minecraft.sloth;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.ai.goal.TemptGoal;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.dimension.DimensionType;
+import net.minecraft.world.level.storage.WorldData;
+
+import java.util.Random;
 
 public class SlothEntity extends Animal {
 
@@ -23,5 +30,11 @@ public class SlothEntity extends Animal {
 	@Override
 	protected void registerGoals() {
 		goalSelector.addGoal(0, new TemptGoal(this, 100, Ingredient.of(ItemInit.EXAMPLE_BLOCK.get()), false));
+	}
+
+	public static boolean canSpawn(EntityType<SlothEntity> entity, LevelAccessor levelAccess, MobSpawnType spawnType, BlockPos pos, Random random)
+	{
+		return checkAnimalSpawnRules(entity, levelAccess, spawnType, pos, random)
+				&& levelAccess instanceof final Level level;
 	}
 }
