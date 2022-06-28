468f97cad40617016e7171da7b558bbe5ad3630a
Nico Auerswald <auersw.nico@gmail.com>
2022-06-28 16:59:43 +0200

    changed model to qudruped model

--- src/main/java/net/slothsoft/minecraft/sloth/SlothModel.java
+++ src/main/java/net/slothsoft/minecraft/sloth/SlothModel.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 
 import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.QuadrupedModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
@@ -15,19 +16,23 @@ import net.minecraft.client.model.geom.builders.PartDefinition;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 
-public class SlothModel<T extends Entity> extends EntityModel<T> {
+public class SlothModel<T extends Entity> extends QuadrupedModel<T> {
 	// This layer location should be baked with EntityRendererProvider.Context in
 	// the entity renderer and passed into this model's constructor
 	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(
 			new ResourceLocation(Constants.MOD_ID, "slothmodel"), "main");
 	private final ModelPart sloth;
 
-	public SlothModel(ModelPart root) {
+	public SlothModel(ModelPart root)
+	{
+		super(root, false, 0f, 1F, 1F, 1F, 12);
+
 		this.sloth = root.getChild("sloth");
 	}
 
+
 	public static LayerDefinition createBodyLayer() {
-		MeshDefinition meshdefinition = new MeshDefinition();
+		MeshDefinition meshdefinition = QuadrupedModel.createBodyMesh(12, CubeDeformation.NONE);
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
 		PartDefinition sloth = partdefinition.addOrReplaceChild("sloth", CubeListBuilder.create(),
@@ -48,28 +53,28 @@ public class SlothModel<T extends Entity> extends EntityModel<T> {
 		PartDefinition legs = sloth.addOrReplaceChild("legs", CubeListBuilder.create(),
 				PartPose.offset(0.0F, -2.0F, 0.0F));
 
-		PartDefinition arm_left = legs.addOrReplaceChild("arm_left",
+		PartDefinition arm_left = legs.addOrReplaceChild("left_front_leg",
 				CubeListBuilder.create().texOffs(13, 19)
 						.addBox(0.2F, -1.3333F, -1.6667F, 4.0F, 2.0F, 3.0F, new CubeDeformation(0.0F)).texOffs(24, 19)
 						.addBox(4.2F, -0.3333F, -0.6667F, 4.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(26, 0)
 						.addBox(8.2F, -0.3333F, -0.6667F, 3.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)),
 				PartPose.offset(3.8F, 1.3333F, -3.3333F));
 
-		PartDefinition leg_left = legs.addOrReplaceChild("leg_left",
+		PartDefinition leg_left = legs.addOrReplaceChild("left_hind_leg",
 				CubeListBuilder.create().texOffs(9, 24)
 						.addBox(0.2F, -1.3333F, -1.3333F, 3.0F, 2.0F, 3.0F, new CubeDeformation(0.0F)).texOffs(0, 3)
 						.addBox(3.2F, -0.3333F, -1.3333F, 3.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(0, 8)
 						.addBox(6.2F, -0.3333F, -0.3333F, 3.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)),
 				PartPose.offset(3.8F, 1.3333F, 3.3333F));
 
-		PartDefinition arm_right = legs.addOrReplaceChild("arm_right",
+		PartDefinition arm_right = legs.addOrReplaceChild("right_front_leg",
 				CubeListBuilder.create().texOffs(16, 14)
 						.addBox(-4.2F, -1.3333F, -1.6667F, 4.0F, 2.0F, 3.0F, new CubeDeformation(0.0F)).texOffs(18, 24)
 						.addBox(-8.2F, -0.3333F, -0.6667F, 4.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(0, 26)
 						.addBox(-11.2F, -0.3333F, -0.6667F, 3.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)),
 				PartPose.offset(-3.8F, 1.3333F, -3.3333F));
 
-		PartDefinition leg_right = legs.addOrReplaceChild("leg_right",
+		PartDefinition leg_right = legs.addOrReplaceChild("right_hind_leg",
 				CubeListBuilder.create().texOffs(0, 21)
 						.addBox(-3.2F, -1.3333F, -1.3333F, 3.0F, 2.0F, 3.0F, new CubeDeformation(0.0F)).texOffs(0, 0)
 						.addBox(-6.2F, -0.3333F, -1.3333F, 3.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(0, 6)
@@ -77,6 +82,10 @@ public class SlothModel<T extends Entity> extends EntityModel<T> {
 				PartPose.offset(-3.8F, 1.3333F, 3.3333F));
 
 		return LayerDefinition.create(meshdefinition, 64, 64);
+	}
+
+	public void prepareMobModel(T p_103687_, float p_103688_, float p_103689_, float p_103690_) {
+		super.prepareMobModel(p_103687_, p_103688_, p_103689_, p_103690_);
 	}
 
 	@Override
